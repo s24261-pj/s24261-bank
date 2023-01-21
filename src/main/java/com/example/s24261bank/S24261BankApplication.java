d0f6ddb9d08d29bf1495f022d84ed8810f70029d
Mateusz Kopczyński <s24261@pjwstk.edu.pl>
2023-01-21 14:54:13 +0100

    Initial commit

---
+++ src/main/java/com/example/s24261bank/S24261BankApplication.java
@@ -0,0 +1,13 @@
+package com.example.s24261bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S24261BankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S24261BankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/s24261bank/S24261BankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.s24261bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S24261BankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
